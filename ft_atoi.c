394104bfa2d9430bef02b31b92b872c5a611720a
Modzie <110020963+M0dzie@users.noreply.github.com>
2022-11-22 20:29:27 +0100

    Add files via upload

--- ft_atoi.c
+++ ft_atoi.c
@@ -6,11 +6,19 @@
 /*   By: thmeyer <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/21 13:20:36 by thmeyer           #+#    #+#             */
-/*   Updated: 2022/11/22 12:12:15 by thmeyer          ###   ########.fr       */
+/*   Updated: 2022/11/22 20:28:34 by thmeyer          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
+
+static int	check_overflow(int sign)
+{
+	if (sign < 0)
+		return (0);
+	return (-1);
+}
+
 /**
  * @brief converts the initial portion of the string pointed to by str to int
  * @param str 
@@ -38,5 +46,17 @@ int	ft_atoi(const char *str)
 		result = result * 10 + str[i] - 48;
 		i++;
 	}
+	if ((result * 10) / 10 != 1)
+		return (check_overflow(sign));
 	return (result * sign);
+}
+
+#include <stdio.h>
+
+int	main(void)
+{
+	char *str;
+	str = "54646846543516468156468";
+	printf("%d\n%d", atoi(str), ft_atoi(str));
+	return (0);
 }
